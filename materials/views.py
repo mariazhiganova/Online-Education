f904873389c3414b6f89566dc0d3c205019a7ceb
maria_zhiganova <my.nik.mariann@gmail.com>
2025-10-11 02:13:15 +0300

    add materials views

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from materials.models import Course, Lesson
+from materials.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonCreateAPIView(CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    queryset = Lesson.objects.all()
