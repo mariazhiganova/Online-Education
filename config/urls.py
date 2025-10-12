1dea2ae56197f2c6c2e8db45debbc4330c229b7d
maria_zhiganova <my.nik.mariann@gmail.com>
2025-10-12 16:01:47 +0300

    add users url

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('materials/', include('materials.urls', namespace='materials')),
+    path('users/', include('users.urls', namespace='users')),
 ]
