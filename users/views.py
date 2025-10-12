fbdfc837fcdba167af16d8a1a8091647b6fe1816
maria_zhiganova <my.nik.mariann@gmail.com>
2025-10-13 00:58:02 +0300

    fixed

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
-from users.models import AbstractUser
+from users.models import CustomUser
 from users.serializers import UserSerializer
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
-    queryset = AbstractUser.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
