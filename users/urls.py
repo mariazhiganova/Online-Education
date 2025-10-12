6a19f5383f35477e88edf356a394afd2790f6638
maria_zhiganova <my.nik.mariann@gmail.com>
2025-10-13 00:59:25 +0300

    fixed

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from users.apps import UsersConfig
 from users.views import UserUpdateAPIView
+
+app_name = UsersConfig.name
 
 urlpatterns = [
     path('profile/<int:pk>/update/', UserUpdateAPIView.as_view(), name='profile_update'),
