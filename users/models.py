0b6fef473b44c87485988ec91c548158d60efefc
maria_zhiganova <my.nik.mariann@gmail.com>
2025-10-13 18:14:05 +0300

    add new model Payments

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from rest_framework.exceptions import ValidationError
+
+from materials.models import Course, Lesson
 
 
 class CustomUser(AbstractUser):
@@ -35,3 +38,30 @@ class CustomUser(AbstractUser):
     class Meta:
         verbose_name = 'пользователь'
         verbose_name_plural = 'пользователи'
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('card', 'Безналичная оплата'),
+    ]
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Пользователь')
+    payment_time = models.DateTimeField(auto_now_add=True, verbose_name='Время оплаты')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Курс')
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Урок')
+    sum = models.PositiveIntegerField(verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES, verbose_name='Способ оплаты')
+
+    class Meta:
+        verbose_name = 'платёж'
+        verbose_name_plural = 'платёжи'
+
+    def __str__(self):
+        return f"Платеж {self.user} - {self.sum} руб."
+
+    def clean(self):
+        if self.course and self.lesson:
+            raise ValidationError('Необходимо выбрать одно: или курс или урок')
+        if not self.course and not self.lesson:
+            raise ValidationError('Необходимо указать что-то одно: курс или урок')
