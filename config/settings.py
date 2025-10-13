f70830882ef4742fcaa8a78acf78a8530efaf76d
maria_zhiganova <my.nik.mariann@gmail.com>
2025-10-13 18:14:36 +0300

    add django-filter

--- config/settings.py
+++ config/settings.py
@@ -43,10 +43,19 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
 
     "users",
     "materials",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ]
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
