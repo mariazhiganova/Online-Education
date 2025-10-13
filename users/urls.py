221fed92fc68279ba2aad7906e59326674272871
maria_zhiganova <my.nik.mariann@gmail.com>
2025-10-13 18:15:23 +0300

    add new url to profile details

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserUpdateAPIView
+from users.views import UserUpdateAPIView, PaymentCreateAPIView, PaymentListAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('profile/<int:pk>/update/', UserUpdateAPIView.as_view(), name='profile_update'),
+
+    path('payment/create/', PaymentCreateAPIView.as_view(), name='payment_create'),
+    path('payment/list/', PaymentListAPIView.as_view(), name='payment_list'),
 ]
