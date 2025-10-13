d9966f05b74e3a8c8ef11e3ce999fd06f59f3dcc
maria_zhiganova <my.nik.mariann@gmail.com>
2025-10-13 18:15:18 +0300

    add new view to profile details

--- users/views.py
+++ users/views.py
@@ -1,8 +1,29 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
-from users.models import CustomUser
-from users.serializers import UserSerializer
+from rest_framework.filters import OrderingFilter
+
+from users.models import CustomUser, Payment
+from users.serializers import UserSerializer, PaymentSerializer
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+
+class PaymentCreateAPIView(generics.CreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ['course', 'lesson', 'payment_method']
+    ordering_fields = ['payment_time']
