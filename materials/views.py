588f7c862a18a567c5bebce700c7c24abe3ee5ac
maria_zhiganova <my.nik.mariann@gmail.com>
2025-10-16 02:36:58 +0300

    add owner in models lesson and course

--- materials/views.py
+++ materials/views.py
@@ -3,31 +3,52 @@ from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIVie
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModerator, IsOwner
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [~IsModerator]
+        if self.action == 'destroy':
+            self.permission_classes = [~IsModerator, IsOwner]
+        elif self.action in ['update', 'list', 'retrieve']:
+            self.permission_classes = [IsModerator]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [~IsModerator]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class LessonListAPIView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsModerator, IsOwner]
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsModerator, IsOwner]
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsModerator, IsOwner]
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [~IsModerator, [~IsModerator]]
