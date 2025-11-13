10c26090e8a06c2b81e14a00cad42dc153083905
maria_zhiganova <my.nik.mariann@gmail.com>
2025-11-13 22:00:03 +0300

    LAST fixes in tests

--- users/permissions.py
+++ users/permissions.py
@@ -10,4 +10,4 @@ class IsModerator(permissions.BasePermission):
 class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return obj == request.user
+        return obj.owner == request.user
