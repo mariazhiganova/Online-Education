bfbbddd91f12d2fd3b6e8c75fea3e353aef959ec
maria_zhiganova <my.nik.mariann@gmail.com>
2025-11-13 20:55:31 +0300

    db for tests fixes

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import sys
 from datetime import timedelta
 from pathlib import Path
 
@@ -185,3 +186,9 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
+    }
